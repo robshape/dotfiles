cf925f07ad3bc8442ec7313302d40799aab09a73
robshape <robshape@users.noreply.github.com>
2024-10-28 13:52:37 +0100

    update

--- setup.zsh
+++ setup.zsh
@@ -1,9 +1,31 @@
 #!/bin/zsh
 set -euo pipefail
 
+echo "[*] LINKING FILES"
+if ! [[ -L "$HOME/.zshrc" ]]; then
+  ## Git
+  ln -fs "$PWD/configs/.gitconfig" "$HOME"
+  ln -fs "$PWD/configs/.gitignore" "$HOME"
+
+  ## Vim
+  VIM_DIRECTORY="$HOME/.vim/"
+  if ! [[ -d "$VIM_DIRECTORY" ]]; then
+    mkdir "$VIM_DIRECTORY"
+  fi
+  ln -fs "$PWD/configs/.tmux.conf" "$HOME"
+  ln -fs "$PWD/configs/.vimrc" "$HOME"
+  ln -fs "$PWD/configs/coc-settings.json" "$VIM_DIRECTORY"
+  ln -fs "$PWD/UltiSnips/" "$VIM_DIRECTORY"
+
+  ## Terminal
+  ln -fs "$PWD/configs/.zshrc" "$HOME"
+  source "$HOME/.zshrc"
+fi
+echo
+
 echo "[*] INSTALLING TOOLS"
 ## Xcode Command Line Developer Tools
-if [[ ! $(xcode-select --install 2>&1 | grep installed) ]]; then
+if ! [[ $(xcode-select --install 2>&1 | grep installed) ]]; then
   xcode-select --install
   echo "Rerun this script after Xcode Command Line Developer Tools has finished installing"
   exit
@@ -25,20 +47,4 @@ brew install ripgrep
 brew install tmux
 brew install vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo
-
-echo "[*] LINKING FILES"
-## Git
-ln -fs "$PWD/configs/.gitconfig" "$HOME"
-ln -fs "$PWD/configs/.gitignore" "$HOME"
-
-## Terminal
-ln -fs "$PWD/configs/.zshrc" "$HOME"
-
-## Vim
-VIM_DIRECTORY="$HOME/.vim/"
-ln -fs "$PWD/configs/.tmux.conf" "$HOME"
-ln -fs "$PWD/configs/.vimrc" "$HOME"
-ln -fs "$PWD/configs/coc-settings.json" "$VIM_DIRECTORY"
-ln -fs "$PWD/UltiSnips/" "$VIM_DIRECTORY"
 echo
